5354917de2d717b17dbee80dd456c77f2d9c4277
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2022-11-18 07:58:49 +0530

    ui module barebones

---
+++ reconciliation-engine-ui/src/test/kotlin/com/taxreco/recon/reconciliationui/ReconciliationEngineUiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.taxreco.recon.reconciliationui
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReconciliationEngineUiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
